679a0dfa340bd8ea4b2509d4b362e5057f4005af
IStallcup <stallcui@oregonstate.edu>
2016-01-22 02:30:17 -0800

    Hopefully added deck stuff right this time

--- Assignment1/src/main/java/models/board.java
+++ Assignment1/src/main/java/models/board.java
@@ -31,55 +31,13 @@ public class board implements Serializable
         }
     }
 
-    //"legacy code"
-    /*
-    //adds the two of spades (default card?) to the column colToAddTo
-    public void addDefaultCardToColumn(int colToAddTo) {
-
-        card cardToAdd = new card();
-        boardList.get(colToAddTo).add(cardToAdd);
-    }*/
-
     //adds a custom card to the column colToAddTo
     public void addCardToColumn(int colToAddTo, card cardToAdd)
     {
         boardList.get(colToAddTo).add(cardToAdd);
     }
 
-    //remove implemented elsewhere
-    /*
-    //checks the first card in inputted columns for number removability (will be expanded to check top card in column)
-    public boolean checkNums(int checkCol, int removeCol)
-    {
-        if (boardList.get(0).get(0).num > boardList.get(0).get(1).num)
-        {
-            return true;
-        }
-        else return false;
-    }*/
 
-    //remove implemented elsewhere
-    /*
-    //checks the first card in inputted columns for suit removability (will be expanded to check top card in column)
-    public boolean checkSuits(int checkCol, int removeCol)
-    {
-        if (boardList.get(checkCol).get(0).suit == boardList.get(removeCol).get(0).suit)
-        {
-            return true;
-        }
-        else return false;
-    }*/
-
-    //remove implemented elsewhere
-    /* //checks the first card in inputted columns for removability (will be expanded to check top card in column)
-    public boolean checkIfRemovable(int checkCol, int removeCol)
-    {
-        if (checkSuits(checkCol, removeCol) && checkNums(checkCol, removeCol))
-        {
-            return true;
-        }
-        else return false;
-    }*/
 
     public card getCardFromDeck()
     {
