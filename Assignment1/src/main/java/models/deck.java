534c3e5c2896cbc2798c117e51e1bf105b549881
Isaac Stallcup <stallcui@oregonstate.edu>
2016-01-22 02:43:13 -0800

    Update deck.java

--- Assignment1/src/main/java/models/deck.java
+++ Assignment1/src/main/java/models/deck.java
@@ -4,14 +4,5 @@ package models;
  * Created by Isaac on 1/21/2016.
  */
 public class deck {
-    card[] deckArray = new card[52];
-
-
-    public deck() {
-        for (int i = 0; i < 52; ++i) {
-            if (i / 13 == 1) {
-                deckArray[i].setSuit('C');
-            }
-        }
-    }
+    
 }
